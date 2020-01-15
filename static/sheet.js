0401ed432fe8461f5992c71d2b59dc1e114e230a
msenkpiel <marco.senkpiel@gmail.com>
2020-01-15 15:18:26 +0100

    app file refactored

--- static/sheet.js
+++ static/sheet.js
@@ -1,35 +1,38 @@
-const areas = document.querySelectorAll('textarea');
-const inputs = document.querySelectorAll('input.hoursOverall');
-const sheetForm = document.getElementById('sheetForm');
-const overallHours = document.getElementById('overallHours');
-const notice = document.getElementById('notice');
-const totalHoursInputList = document.querySelectorAll('input.totalHours');
+const App = {};
+App.run = function () {
+    console.log('run app');
 
-areas.forEach((element) => {
-    element.addEventListener('change', (event) => collectData());
-});
+    const areas = document.querySelectorAll('textarea');
+    const inputs = document.querySelectorAll('input.hoursOverall');
+    const sheetForm = document.getElementById('sheetForm');
+    const overallHours = document.getElementById('overallHours');
+    const notice = document.getElementById('notice');
+    const totalHoursInputList = document.querySelectorAll('input.totalHours');
 
-inputs.forEach((element) => {
-    element.addEventListener('change', (event) => collectData());
-});
+    areas.forEach((element) => {
+        element.addEventListener('change', (event) => collectData());
+    });
 
-notice.addEventListener('change', (event) => collectData());
+    inputs.forEach((element) => {
+        element.addEventListener('change', (event) => collectData());
+    });
 
-function collectData() {
-    console.log('collect Data');
-    sheetForm.submit();
-}
+    notice.addEventListener('change', (event) => collectData());
 
-
-
-// calculate overall hours
-let hours = 0;
-totalHoursInputList.forEach((element) => {
-    let val = element.value;
-    if(val !== '') {
-        hours += parseInt(val);
+    function collectData() {
+        console.log('collect Data');
+        sheetForm.submit();
     }
-});
-overallHours.innerText = hours.toString();
 
-console.log('run');
+    // calculate overall hours
+    let hours = 0;
+    totalHoursInputList.forEach((element) => {
+        let val = element.value;
+        if(val !== '') {
+            hours += parseInt(val);
+        }
+    });
+    overallHours.innerText = hours.toString();
+};
+
+document.addEventListener("DOMContentLoaded", (event) => App.run());
