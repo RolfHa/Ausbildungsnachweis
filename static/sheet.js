d0d7ccf262a08cc5c373566dea82e38477f128fa
msenkpiel <marco.senkpiel@gmail.com>
2020-01-15 12:13:57 +0100

    added overall hours calculation

--- static/sheet.js
+++ static/sheet.js
@@ -1,6 +1,8 @@
 const areas = document.querySelectorAll('textarea');
 const inputs = document.querySelectorAll('input.hoursOverall');
 const sheetForm = document.getElementById('sheetForm');
+const overallHours = document.getElementById('overallHours');
+const totalHoursInputList = document.querySelectorAll('input.totalHours');
 
 areas.forEach((element) => {
     element.addEventListener('change', (event) => {
@@ -18,3 +20,17 @@ function collectData() {
     console.log('collect Data');
     sheetForm.submit();
 }
+
+
+
+// calculate overall hours
+let hours = 0;
+totalHoursInputList.forEach((element) => {
+    let val = element.value;
+    if(val !== '') {
+        hours += parseInt(val);
+    }
+});
+overallHours.innerText = hours.toString();
+
+console.log('run');
