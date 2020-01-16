b6cda878b423129a8ea1789692844e015ac5acb0
msenkpiel <marco.senkpiel@gmail.com>
2020-01-16 08:30:07 +0100

    event listener refactoring

--- static/sheet.js
+++ static/sheet.js
@@ -2,22 +2,13 @@ const App = {};
 App.run = function () {
     console.log('run app');
 
-    const areas = document.querySelectorAll('textarea');
-    const inputs = document.querySelectorAll('input.hoursOverall');
+    const inputs = document.querySelectorAll(['input[type=number]','input[type=text]','textarea', 'select']);
     const sheetForm = document.getElementById('sheetForm');
-    const overallHours = document.getElementById('overallHours');
-    const notice = document.getElementById('notice');
     const totalHoursInputList = document.querySelectorAll('input.totalHours');
-
-    areas.forEach((element) => {
-        element.addEventListener('change', (event) => collectData());
-    });
 
     inputs.forEach((element) => {
         element.addEventListener('change', (event) => collectData());
     });
-
-    notice.addEventListener('change', (event) => collectData());
 
     function collectData() {
         console.log('collect Data');
