c0a8ddfb99f3b8769f60b05f34c44399cc8c959b
msenkpiel <marco.senkpiel@gmail.com>
2020-02-03 11:42:21 +0100

    __PHP_Incomplete_Class error fixed  see: https://www.macuser.de/threads/ein-problem-mit-session-php-incomplete-class-object.431681/#post-4921533

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-session_start();
 include 'config.php';
 spl_autoload_register(function ($class) {
     include 'class/' . $class . '.php';
 });
+
+session_start();
 
 if(isset($_SESSION) && isset($_SESSION['auth']) && $_SESSION['auth'] === true){
     $view = "view/sheet.php";
